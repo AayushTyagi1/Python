1260551da8f89eee9de5a6f0e27a4618f1c1d206
Aayush Tyagi <tyagiaayush5@gmail.com>
2019-08-19 14:16:39 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,8 @@
+a="Hi %s How are you %s!"
+b=('Aayush','man')
+print(a % b)
+name=input("Enter your name:")
+age=int(input("Enter age:"))
+c="Hi %s you age is %d"
+print(c % (name,age))
+print(a.find('How'))
